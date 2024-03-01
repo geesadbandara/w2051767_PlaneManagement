ee76a03605f275172d585235667a70bb212e1291
Geesad Bandara <chamithu.20230782@iit.ac.lk>
2024-03-01 18:31:19 +0530

    created class ticket and added getters and setter

---
+++ src/Ticket.java
@@ -0,0 +1,40 @@
+public class Ticket {
+    String row,name,surname,email;
+    int seatNo;
+    double price;
+    Person ticketOwner = new Person(name,surname,email);
+    public Ticket(String row,int seatNo,double price,Person ticketOwner){
+        this.row = row;
+        this.seatNo = seatNo;
+        this.price = price;
+        this.ticketOwner = ticketOwner;
+    }
+
+    public void setTicketOwner(Person ticketOwner) {
+        this.ticketOwner = ticketOwner;
+    }
+    public Person getTicketOwner() {
+        return ticketOwner;
+    }
+
+    public void setRow(String row) {
+        this.row = row;
+    }
+    public String getRow() {
+        return row;
+    }
+
+    public void setSeatNo(int seatNo) {
+        this.seatNo = seatNo;
+    }
+    public int getSeatNo() {
+        return seatNo;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+    public double getPrice() {
+        return price;
+    }
+}
